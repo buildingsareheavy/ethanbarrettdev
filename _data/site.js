72c2f786d2a1fb1191c0d3d6e52b3386a2d3083c
Ethan Barrett <hello@buildingsareheavy.com>
2026-04-24 03:26:53 -0600

    added meta tags and favicon to layout and added site variables

---
+++ _data/site.js
@@ -0,0 +1,7 @@
+export default {
+  url: "https://ethanbarrett.dev",
+  title: "EthanBarrett.dev",
+  description:
+    "Ethan Barrett is a front-end developer specializing in accessibility, usability, and long-term maintainability.",
+  year: new Date().getFullYear(),
+};
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,4 @@
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css/style.css");
+  eleventyConfig.addPassthroughCopy("assets");
 }
